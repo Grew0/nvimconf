f570422ea2682dbaf2556b4c134a92245b7d3a49
Chikunov Danila <chikunov.danila@h-partners.com>
2025-07-22 12:46:33 +0300

    Some alpha icons and terminal

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -20,11 +20,12 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-			dashboard.button( "f", "  > Find file", "Telescope find_files<CR>"),
-			dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+			dashboard.button( "n", "  > New file" , ":ene<CR>"),
+			dashboard.button( "f", "󰈞  > Find file", "Telescope find_files<CR>"),
+			dashboard.button( "r", "󱋡  > Recent file"   , ":Telescope oldfiles<CR>"),
 			dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-			dashboard.button( "q", "X  > Quit NVIM", ":qa<CR>"),
+			dashboard.button( "t", "  > Terminal" , ":ene<CR>:terminal<CR>i"),
+			dashboard.button( "q", "󰜺  > Quit NVIM", ":qa<CR>"),
 		}
 
         require'alpha'.setup(dashboard.opts)
