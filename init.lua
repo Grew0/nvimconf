79d6d4c9678ce29661c957ec15e80a17587369d3
Чикунов ДС <dan123dan5@mail.ru>
2025-07-27 15:02:38 +0300

    Autoclose added

--- init.lua
+++ init.lua
@@ -13,8 +13,8 @@ require("config.functions")
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smarttab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+-- vim.opt.autoindent = true
+-- vim.opt.smartindent = true
 
 -- numbers
 vim.opt.number = true
