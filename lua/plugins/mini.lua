66183cb5312dd20fda5178a00975fc54330505c1
Chikunov Danila <chikunov.danila@h-partners.com>
2025-09-29 12:13:35 +0300

    Mini pair fix for quotes

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,13 +6,19 @@ return {
 		vim.opt.shiftwidth = 4
 		vim.opt.smarttab = true
 		require('mini.pairs').setup({
+			modes = { insert = true, command = true, terminal = false },
 			mappings = {
-				['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\][^%w]' },
-				['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\][^%w]' },
-				['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\][^%w]' },
-				['"'] = { action = 'open', pair = '""', neigh_pattern = '[^%w]' },
-				["'"] = { action = 'open', pair = "''", neigh_pattern = '[^%w]' },
-				['`'] = { action = 'open', pair = '``', neigh_pattern = '[^%w]' },
+				['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+				['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+				['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+
+				[')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+				[']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+				['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+
+				['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+				["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
+				['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
 			},
 		})
 	end
