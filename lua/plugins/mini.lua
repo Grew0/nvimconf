5a6c62698ba5e6b7a0f25de56948110b238103e8
Чикунов ДС <dan123dan5@mail.ru>
2025-10-12 20:42:52 +0300

    Correcting pattern for minipairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -8,17 +8,17 @@ return {
 		require('mini.pairs').setup({
 			modes = { insert = true, command = true, terminal = false },
 			mappings = {
-				['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-				['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-				['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+				['('] = { action = 'open', pair = '()', neigh_pattern = '.[^%a\\(]' },
+				['['] = { action = 'open', pair = '[]', neigh_pattern = '.[^%a\\[]' },
+				['{'] = { action = 'open', pair = '{}', neigh_pattern = '.[^%a\\{]' },
 
 				[')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
 				[']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
 				['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
 
-				['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-				["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-				['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+				['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '.[^%a\\]', register = { cr = false } },
+				["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '.[^%a\\]', register = { cr = false } },
+				['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '.[^%a\\]', register = { cr = false } },
 			},
 		})
 	end
