f0d531809109da59b10b8a3947e991814d720ca2
Chikunov Danila <chikunov.danila@h-partners.com>
2025-10-16 11:30:35 +0300

    Lsp goto in new tab for gD

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,7 @@
 local on_attach = function(client, bufnr)
 	local opts = { buffer = bufnr }
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- Go to definition
+	vim.keymap.set('n', 'gD', "<c-w><c-]><c-w>T", { desc = 'LSP definitions in new Tab' })
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)       -- Show docs
 end
 local configs = {'clangd', 'pyright', 'bashls', 'ts_ls'}
