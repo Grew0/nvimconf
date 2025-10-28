a7ab2c4c09f4a8643af826968f69d309180d72b6
Чикунов ДС <dan123dan5@mail.ru>
2025-10-28 13:40:45 +0300

    Deleted lua snip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,8 +18,6 @@ return {
 			"hrsh7th/cmp-nvim-lsp",     -- LSP source for nvim-cmp
 			"hrsh7th/cmp-buffer",       -- Buffer completions
 			"hrsh7th/cmp-path",         -- Path completions
-			"saadparwaiz1/cmp_luasnip", -- Snippets support
-			"L3MON4D3/LuaSnip",         -- Snippet engine
 			'hrsh7th/cmp-cmdline',
 			'hrsh7th/cmp-vsnip',
 			'hrsh7th/vim-vsnip'
