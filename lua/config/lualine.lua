f6fd3409ca924a7945200176ef90b7bdeae815a3
Чикунов ДС <dan123dan5@mail.ru>
2026-02-04 12:32:54 +0300

    Faster windows

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -4,32 +4,7 @@ require('lualine').setup {
     theme = 'auto',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
     always_show_tabline = false,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-      refresh_time = 16, -- ~60fps
-      events = {
-        'WinEnter',
-        'BufEnter',
-        'BufWritePost',
-        'SessionLoadPost',
-        'FileChangedShellPost',
-        'VimResized',
-        'Filetype',
-        'CursorMoved',
-        'CursorMovedI',
-        'ModeChanged',
-      },
-    }
   },
   sections = {
     lualine_a = {'mode'},
@@ -40,12 +15,8 @@ require('lualine').setup {
     lualine_z = {'location'}
   },
   inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
     lualine_c = {'filename'},
     lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
   },
   tabline = {
     lualine_a={ {
@@ -54,7 +25,4 @@ require('lualine').setup {
 		max_length=vim.o.columns
 	} },
   },
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
 }
