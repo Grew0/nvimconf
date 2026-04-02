33c1bc5bc4d7b60addae80fb940983bebca4f484
Chikunov Danila <chikunov.danila@h-partners.com>
2026-04-02 11:10:16 +0300

    Better telescope <C-q>

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,4 +1,6 @@
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
@@ -19,11 +21,31 @@ local function open_marked_as_tabs(prompt_bufnr)
     vim.cmd('tabnext')
 end
 
+
+local function smart_qflist(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+
+  if vim.tbl_isempty(multi) then
+    actions.send_to_qflist(prompt_bufnr)
+  else
+    actions.send_selected_to_qflist(prompt_bufnr)
+  end
+
+  actions.open_qflist(prompt_bufnr)
+end
+
 require('telescope').setup({
   defaults = {
     mappings = {
-      i = { ["<C-t>"] = open_marked_as_tabs },
-      n = { ["<C-t>"] = open_marked_as_tabs },
+      i = { 
+		  ["<C-t>"] = open_marked_as_tabs,
+		  ["<C-q>"] = smart_qflist,
+	  },
+      n = {
+		  ["<C-t>"] = open_marked_as_tabs,
+		  ["<C-q>"] = smart_qflist,
+	  },
     }
   }
 })
